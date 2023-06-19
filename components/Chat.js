0e47d7734a65fe9f10040f2c10002270e8cbe2a5
Maya-Bitter <mayabitter@gmail.com>
2023-06-19 11:08:30 +0200

    messages-code

--- components/Chat.js
+++ components/Chat.js
@@ -4,6 +4,27 @@ import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
   const { name, color } = route.params;
+  const [messages, setMessages] = useState([]);
+  const onSend = (newMessages) => {
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, newMessages)
+    );
+  };
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: "Hello developer",
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: "React Native",
+          avatar: "https://placeimg.com/140/140/any",
+        },
+      },
+    ]);
+  }, []);
 
   useEffect(() => {
     navigation.setOptions({ title: name });
@@ -15,6 +36,16 @@ const Chat = ({ route, navigation }) => {
     </View>
   );
 };
+
+return (
+  <GiftedChat
+    messages={messages}
+    onSend={(messages) => onSend(messages)}
+    user={{
+      _id: 1,
+    }}
+  />
+);
 
 const styles = StyleSheet.create({
   container: {
