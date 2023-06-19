acc6838a839278227d5f8f1aeedb9919590f1bbe
Maya-Bitter <mayabitter@gmail.com>
2023-06-19 11:35:21 +0200

    Fixing the Android Keyboard

--- components/Chat.js
+++ components/Chat.js
@@ -1,5 +1,11 @@
 import { useState, useEffect } from "react";
-import { StyleSheet, View, Text } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+} from "react-native";
 import { GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
@@ -33,26 +39,32 @@ const Chat = ({ route, navigation }) => {
   return (
     <View style={[styles.container, { backgroundColor: color }]}>
       <Text>You are in the Chat</Text>
+      <GiftedChat
+        messages={messages}
+        onSend={(messages) => onSend(messages)}
+        user={{
+          _id: 1,
+        }}
+      />
+      {Platform.OS === "android" ? (
+        <KeyboardAvoidingView behavior="height" />
+      ) : null}
     </View>
   );
 };
 
-return (
-  <GiftedChat
-    messages={messages}
-    onSend={(messages) => onSend(messages)}
-    user={{
-      _id: 1,
-    }}
-  />
-);
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
 });
+
+//const styles = StyleSheet.create({
+//  container: {
+//    flex: 1,
+//    justifyContent: "center",
+//    alignItems: "center",
+//  },
+//});
 
 export default Chat;
