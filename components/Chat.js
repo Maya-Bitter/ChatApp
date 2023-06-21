33958a0736f700638b5be1dc0381e3321ceda6d7
Maya-Bitter <mayabitter@gmail.com>
2023-06-21 23:14:24 +0200

    deletes style

--- components/Chat.js
+++ components/Chat.js
@@ -109,12 +109,4 @@ const styles = StyleSheet.create({
   },
 });
 
-//const styles = StyleSheet.create({
-//  container: {
-//    flex: 1,
-//    justifyContent: "center",
-//    alignItems: "center",
-//  },
-//});
-
 export default Chat;
